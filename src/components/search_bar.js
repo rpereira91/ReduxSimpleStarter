5970afe433bb37f867c2bc3c736778adc0ad7150
rpereira91 <rp09rl@brocku.ca>
2018-07-05 16:15:50 -0700

    States added

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,4 +1,29 @@
-import React from 'react'
-const SearchBar = () => {
-    return <input />
-};
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+    //constructor is called by default at every instance
+    constructor(props){
+        //calls the parent method
+        super(props);
+        //create a new object and assignt it to this.state, this records the search term to state
+        //state will only be changed like this in the constructor
+        this.state = { term: ''};
+    }
+    render() {
+        //whenever the user interacts with this input bar it emits a change method
+        //to envoke JS code write "on" then th name then the name of the function
+        //state is set to the term
+        return <input onChange={event => this.setState({term: event.target.value})} />;
+
+        //this is the old way to use the event, calling the onInputChange method
+        // return <input onChange={this.onInputChange} />;
+    }
+    //either handle or on then the name of the event, is the naming convention
+    //it's passed an event object because of the event change
+    //dep
+    // onInputChange(event) {
+    //     console.log(event.target.value)
+    // }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+//since its a file we wrote we nede to go into the proper sub folder
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBwYGFBUT4lKrurMIBLJYxcWR-9flhT4is';
 
 
 //create a new component. Component should produce some HTML
 const App = () => {
-    return <div> HI!</div>;
+    return (
+    <div> 
+        <SearchBar/ >
+    </div>);
 }
 //put the HTML in the DOM somehow
 //creates a react element for the DOM to render, by wrapping it with JSX tags
